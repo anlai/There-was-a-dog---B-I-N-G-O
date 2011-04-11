0e57728544ee930f348dc0081f40a21b7bb54ed7
Alan Lai <anlai@ucdavis.edu>
2011-04-10 22:25:57 -0700

    transfer all scripts/files to get basic bingo ball page loaded up.

---
+++ Bingo.Web/Controllers/ClientController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+
+namespace Bingo.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the Client class
+    /// </summary>
+    public class ClientController : ApplicationController
+    {
+        public ActionResult Game()
+        {
+            var viewModel = ClientViewModel.Create(Repository);
+            return View(viewModel);
+        }
+    }
+
+    public class ClientViewModel
+    {
+        public static ClientViewModel Create(IRepository repository)
+        {
+            Check.Require(repository != null, "Repository is required.");
+
+            var viewModel = new ClientViewModel();
+
+            return viewModel;
+        }
+    }
+}
