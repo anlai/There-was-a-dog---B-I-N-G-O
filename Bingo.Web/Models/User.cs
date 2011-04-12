f87f09cc1d2c1d3f32580d9e90589190658e0eb5
Scott Kirkland <srkirkland@gmail.com>
2011-04-11 22:03:30 -0700

    updated the project to create user profiles and random game boards

---
+++ Bingo.Web/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using FluentNHibernate.Mapping;
+
+using UCDArch.Core.DomainModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bingo.Web.Models
+{
+    public class User : DomainObject
+    {
+        public virtual string Kerb { get; set; }
+        [Required]
+        public virtual string Name { get; set; }
+        public virtual string BingoBoard { get; set; }
+
+        public virtual GameBoard GetBoard()
+        {
+            var board = new GameBoard {AllNumsAsString = BingoBoard};
+            board.InitFromString();
+
+            return board;
+        }
+    }
+
+    public class UserMap : ClassMap<User>
+    {
+        public UserMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Kerb);
+            Map(x => x.Name);
+            Map(x => x.BingoBoard).Column("Board");
+        }
+    }
+}
