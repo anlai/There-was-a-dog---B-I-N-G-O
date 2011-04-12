191e25d2549789a22d16ab4bd0cf0812c51d714b
Scott Kirkland <srkirkland@gmail.com>
2011-04-11 22:07:55 -0700

    Added sql project

---
+++ Bingo.Sql/Schema Objects/Schemas/dbo/Tables/Users.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]    INT            IDENTITY (1, 1) NOT NULL,
+    [Kerb]  VARCHAR (16)   NOT NULL,
+    [Name]  NVARCHAR (100) NOT NULL,
+    [Board] VARCHAR (256)  NOT NULL
+);
+
