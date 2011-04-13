70fdf5943e7a0775077c47d3620a5ad582ddfa40
Alan Lai <anlai@ucdavis.edu>
2011-04-12 21:13:09 -0700

    added some basic pages

---
+++ Bingo.Web/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿﻿using System.Linq;
+using System.Web.Mvc;
+using Bingo.Web.Models;
+using UCDArch.Web.Attributes;
+
+namespace Bingo.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the Client class
+    /// </summary>
+    public class PlayerController : ApplicationController
+    {
+        public ActionResult Game()
+        {
+            var board = GameBoard.Random();
+
+            return View(board);
+        }
+    }
+}
