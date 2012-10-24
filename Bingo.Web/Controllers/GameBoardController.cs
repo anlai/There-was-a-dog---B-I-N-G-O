3be3c3e4a62a6ae25c5bc70ca93709e222dc5883
anlai <anlai@ucdavis.edu>
2012-10-24 09:43:44 -0700

    added in the game board generator

---
+++ Bingo.Web/Controllers/GameBoardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bingo.Web.Models;
+
+namespace Bingo.Web.Controllers
+{
+    public class GameBoardController : ApplicationController
+    {
+        //
+        // GET: /GameBoard/
+
+        public ActionResult Index()
+        {
+            return View(GameBoard.Random());
+        }
+
+    }
+}
